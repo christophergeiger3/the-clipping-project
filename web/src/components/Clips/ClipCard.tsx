aaec45b731b84319d24228c9c45977f64c209300
christophergeiger3 <christophergeiger3@gmail.com>
2022-04-30 20:01:08 -0400

    Allow deleting a clip on /clips (frontend)

--- web/src/components/Clips/ClipCard.tsx
+++ web/src/components/Clips/ClipCard.tsx
@@ -1,12 +1,28 @@
-import { Link } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { CardActions, Link } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { useCallback, useState } from "react";
 import { Clip } from "./Clips";
 
 // MUI Card displaying clip id, url, start, end, output, status, createdAt, updatedAt
 export default function ClipCard({ clip }: { clip: Clip }) {
+  const [isDeleting, setIsDeleting] = useState(false);
   const clipLink = `/clips/${clip._id}`;
+
+  const deleteClip = useCallback(async () => {
+    setIsDeleting(true);
+    const response = await axios.delete(
+      `http://localhost:3000/clips/${clip._id}`
+    );
+    console.log(response.data);
+    if (response.data.message === "success") {
+      console.log("deleted clip", clip._id);
+    }
+    setIsDeleting(false);
+  }, [clip._id]);
   return (
     <Card variant="outlined">
       <CardContent>
@@ -23,6 +39,16 @@ export default function ClipCard({ clip }: { clip: Clip }) {
         {/* Temporary, for debugging: */}
         <Typography>{clip._id}</Typography>
       </CardContent>
+      <CardActions>
+        <LoadingButton
+          variant="outlined"
+          color="secondary"
+          loading={isDeleting}
+          onClick={deleteClip}
+        >
+          Delete
+        </LoadingButton>
+      </CardActions>
     </Card>
   );
 }
