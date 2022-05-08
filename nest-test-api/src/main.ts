8de2a353e26cb921e5351177133f443ce836a70d
christophergeiger3 <christophergeiger3@gmail.com>
2022-05-08 13:53:16 -0400

    Add sketch of swagger/open-api endpoint (/api)

--- nest-test-api/src/main.ts
+++ nest-test-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The Clipping Project API')
+    .setDescription('An API for creating clips from video URLs')
+    .setVersion('1.0')
+    .addTag('clips')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
