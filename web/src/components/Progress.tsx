0e7be52dbfd424cfa515217c3205d432571e8ce4
christophergeiger3 <christophergeiger3@gmail.com>
2022-05-14 12:56:51 -0400

    Reformat progress SSE calls to new api

--- web/src/components/Progress.tsx
+++ web/src/components/Progress.tsx
@@ -2,14 +2,19 @@ import { LinearProgress, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+interface ProgressResponse {
+  progress: number;
+}
+
 const useEventSource = (url: string) => {
   const [data, updateData] = useState<number | null>();
 
   useEffect(() => {
     const source = new EventSource(url);
 
-    source.onmessage = function logEvents(event) {
-      updateData(JSON.parse(event.data));
+    source.onmessage = ({ data }: MessageEvent<string>) => {
+      const { progress } = JSON.parse(data) as ProgressResponse;
+      updateData(progress);
     };
   }, [url]);
 
@@ -18,7 +23,7 @@ const useEventSource = (url: string) => {
 
 export default function Progress() {
   const { id } = useParams<{ id: string }>();
-  const data = useEventSource(`http://localhost:3000/clips/progress/${id}`);
+  const data = useEventSource(`http://localhost:3000/clips/${id}/progress`);
 
   // Null response from event source means the clip is not processing
   if (data === null) {
