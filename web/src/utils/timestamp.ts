8508fb56fdaa2cde50d8bf7e9d22fb888c20b9cd
christophergeiger3 <christophergeiger3@gmail.com>
2022-05-20 21:53:41 -0400

    Update timestamp functions

--- web/src/utils/timestamp.ts
+++ web/src/utils/timestamp.ts
@@ -1,8 +1,14 @@
-
 export function convertSecondsToTimestamp(seconds: number) {
-  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor(seconds / 60) % 60;
   const secondsLeft = seconds % 60;
-  return `${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${secondsLeft
+      .toString()
+      .padStart(2, "0")}`;
+  } else {
+    return `${minutes}:${secondsLeft.toString().padStart(2, "0")}`;
+  }
 }
 
 export function convertMillisecondsToTimestamp(
