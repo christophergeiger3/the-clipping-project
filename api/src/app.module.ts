1ca4b3382fb8c0e3d084d4c5f9267af9a44eacae
christophergeiger3 <christophergeiger3@gmail.com>
2022-05-25 21:19:51 -0400

    Add validation for .env file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -10,10 +10,25 @@ import { AnalyzeService } from './analyze/analyze.service';
 import { AnalyzeModule } from './analyze/analyze.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import * as Joi from 'joi';
+
+const environmentValidationSchema = Joi.object({
+  PORT: Joi.number().default(4190),
+  ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('production'),
+  HOST: Joi.string().default('http://localhost:4190'),
+  DATABASE_URL: Joi.string().default('mongodb://mongo/the-clipping-project'),
+  TEST_DATABASE_URL: Joi.string().default(
+    'mongodb://mongo/the-clipping-project-test',
+  ),
+});
 
 @Module({
   imports: [
-    ConfigModule.forRoot(), // for .env files
+    ConfigModule.forRoot({
+      validationSchema: environmentValidationSchema,
+    }), // for .env files
     ClipsModule,
     EventEmitterModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
