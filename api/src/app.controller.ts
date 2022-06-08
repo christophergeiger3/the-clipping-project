cca3bc6d82dc78c216d29b24a1d9bb225ea513bc
christophergeiger3 <christophergeiger3@gmail.com>
2022-06-08 18:27:41 -0400

    Add yt-dlp version check to /

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getStatus(): string {
+  getStatus(): Promise<string> {
     return this.appService.getStatus();
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { exec as callbackExec } from 'child_process';
+import { promisify } from 'util';
+
+const exec = promisify(callbackExec);
 
 @Injectable()
 export class AppService {
-  getStatus(): string {
+  async getStatus(): Promise<string> {
+    await exec('yt-dlp --version');
     return 'The clips API is up and running!';
   }
 }
