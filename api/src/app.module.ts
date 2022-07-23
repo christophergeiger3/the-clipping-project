07c7eaa8a29fc21e3e0902fbdd7eca6e17c77cb1
christophergeiger3 <christophergeiger3@gmail.com>
2022-07-23 13:37:14 -0400

    Use reasonable env defaults from env.default.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,25 +5,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClipsModule } from './clips/clips.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { AnalyzeController } from './analyze/analyze.controller';
-import { AnalyzeService } from './analyze/analyze.service';
 import { AnalyzeModule } from './analyze/analyze.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import * as Joi from 'joi';
+import {
+  API_URL,
+  DATABASE_URL,
+  ENV,
+  PORT,
+  TEST_DATABASE_URL,
+} from './env.default';
 
 const environmentValidationSchema = Joi.object({
-  PORT: Joi.number().default(4190),
-  ENV: Joi.string()
-    .valid('development', 'production', 'test')
-    .default('production'),
-  API_URL: Joi.string().default('http://localhost:4190'),
-  DATABASE_URL: Joi.string().default('mongodb://mongo/the-clipping-project'),
-  TEST_DATABASE_URL: Joi.string().default(
-    'mongodb://mongo/the-clipping-project-test',
-  ),
+  PORT: Joi.number().default(PORT),
+  ENV: Joi.string().valid('development', 'production', 'test').default(ENV),
+  API_URL: Joi.string().default(API_URL),
+  DATABASE_URL: Joi.string().default(DATABASE_URL),
+  TEST_DATABASE_URL: Joi.string().default(TEST_DATABASE_URL),
 });
 
 @Module({
@@ -32,7 +33,7 @@ const environmentValidationSchema = Joi.object({
       validationSchema: environmentValidationSchema,
     }), // for .env files
     EventEmitterModule.forRoot(),
-    MongooseModule.forRoot(process.env.DATABASE_URL),
+    MongooseModule.forRoot(process.env.DATABASE_URL || DATABASE_URL),
     AnalyzeModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
---
+++ api/src/env.default.ts
@@ -0,0 +1,5 @@
+export const PORT = 4190;
+export const ENV = 'production';
+export const API_URL = 'http://localhost:4190';
+export const DATABASE_URL = 'mongodb://mongo/the-clipping-project';
+export const TEST_DATABASE_URL = 'mongodb://mongo/the-clipping-project-test';
