ced3502da4ac1dee9f2386afa22d380cd99c9f44
christophergeiger3 <christophergeiger3@gmail.com>
2022-07-30 16:24:06 -0400

    Fix undefined `PORT` bug in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PORT } from './env.default';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || PORT);
 }
 bootstrap();
