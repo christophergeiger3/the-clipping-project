b28a79e9e76fb6560fb57290b0f45ee40f7820a9
christophergeiger3 <christophergeiger3@gmail.com>
2022-08-14 16:26:14 -0400

    Show the startEnd slider conditionally

--- vite/src/components/ClippingView/ClippingView.tsx
+++ vite/src/components/ClippingView/ClippingView.tsx
@@ -1,8 +1,10 @@
 import { useCallback, useState } from "react";
-import videojs, { VideoJsPlayer } from "video.js";
+import videojs from "video.js";
 import { toMilliseconds } from "../../utils/timestamp";
 import ClippingControlPanel from "./ClippingControls/ClippingControlPanel";
-import ClipStartEndTimesSlider from "./ClippingControls/ClipStartEndTimesSlider";
+import ClipStartEndTimesSlider, {
+  ClipStartEndTimes,
+} from "./ClippingControls/ClipStartEndTimesSlider";
 import VideoControlPanel from "./ClippingControls/VideoControlPanel";
 import VideoPlayer from "./VideoPlayer";
 import ViewAllClipsButton from "./ViewAllClipsButton";
@@ -10,10 +12,15 @@ import ViewAllClipsButton from "./ViewAllClipsButton";
 const DEFAULT_VIDEO_SRC =
   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
+function isNonNullable<T>(value: T | null | undefined): value is T {
+  return value !== null && value !== undefined;
+}
+
 export default function ClippingView() {
   const [src, _setSrc] = useState(DEFAULT_VIDEO_SRC);
-  const [clipDuration, setClipDuration] = useState<number>(10000);
-  const [[clipStart, clipEnd], setClipStartEnd] = useState([0, clipDuration]);
+  const [clipDuration, setClipDuration] = useState<number>();
+  const [clipStartEndTimes, setClipStartEnd] = useState<ClipStartEndTimes>();
+  const [clipStart, clipEnd] = clipStartEndTimes || [undefined, undefined];
 
   const handleVideoPlayerReady = useCallback<videojs.ReadyCallback>(function (
     this: videojs.Player
@@ -26,15 +33,22 @@ export default function ClippingView() {
   },
   []);
 
+  const showClipStartEndTimesSlider =
+    isNonNullable(clipStart) &&
+    isNonNullable(clipEnd) &&
+    isNonNullable(clipDuration);
+
   return (
     <>
       <VideoPlayer src={src} onVideoPlayerReady={handleVideoPlayerReady} />
-      <ClipStartEndTimesSlider
-        start={clipStart}
-        end={clipEnd}
-        duration={clipDuration}
-        onUpdateStartEndTimes={setClipStartEnd}
-      />
+      {showClipStartEndTimesSlider ? (
+        <ClipStartEndTimesSlider
+          start={clipStart}
+          end={clipEnd}
+          duration={clipDuration}
+          onUpdateStartEndTimes={setClipStartEnd}
+        />
+      ) : null}
       <VideoControlPanel />
       <ClippingControlPanel />
       <ViewAllClipsButton />
