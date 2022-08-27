57d03ec75f0c42fabe852cd4b53a979d1ae7ba4b
christophergeiger3 <christophergeiger3@gmail.com>
2022-08-27 14:35:49 -0400

    Add simple theming

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,7 +1,22 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import ClippingView from "./components/ClippingView/ClippingView";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1e5791",
+    },
+  },
+});
+
 function App() {
-  return <ClippingView />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <ClippingView />
+    </ThemeProvider>
+  );
 }
 
 export default App;
