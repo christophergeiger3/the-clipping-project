5f03e10a4c15dcfe0d5b9405d530a63bc8732244
christophergeiger3 <christophergeiger3@gmail.com>
2022-09-13 19:32:43 -0400

    Add `item={true}` to VideoControlPanel Grid

--- vite/src/components/ClippingView/ClippingControls/VideoControlPanel.tsx
+++ vite/src/components/ClippingView/ClippingControls/VideoControlPanel.tsx
@@ -19,7 +19,7 @@ const PAPER_STYLE: SxProps<Theme> = { padding: 1 };
 
 export default function VideoControlPanel() {
   return (
-    <Grid padding={2}>
+    <Grid item={true} padding={2}>
       <Paper elevation={1} sx={PAPER_STYLE}>
         <Grid container={true} justifyContent="center">
           <Grid container={true} xs={6} justifyContent="center" spacing={3}>
