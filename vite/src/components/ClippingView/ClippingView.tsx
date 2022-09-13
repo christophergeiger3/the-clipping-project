9e02806dbff5551bdec68bd388178f65cccb1311
christophergeiger3 <christophergeiger3@gmail.com>
2022-09-13 19:35:56 -0400

    Add margin to `<ClippingView />`

--- vite/src/components/ClippingView/ClippingView.tsx
+++ vite/src/components/ClippingView/ClippingView.tsx
@@ -30,7 +30,7 @@ export default function ClippingView() {
   ) : null;
 
   return (
-    <Grid container={true} columns={100} pt={2}>
+    <Grid container={true} columns={100} mt={2} mb={2}>
       <Grid item={true} xs={1} md={5} xl={10} />
       <Grid item={true} xs={98} md={90} xl={80}>
         <VideoPlayer src={DEFAULT_CLIP_STATE.src} />
