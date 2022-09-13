0c3fe2a78d2f3a52b9ade8d9e7a4bc5986068003
christophergeiger3 <christophergeiger3@gmail.com>
2022-09-13 19:36:36 -0400

    Wrap `<ViewAllClipsButton />` in `<Grid />`

--- vite/src/components/ClippingView/ViewAllClipsButton.tsx
+++ vite/src/components/ClippingView/ViewAllClipsButton.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 export default function ViewAllClipsButton() {
   return (
-    <Button startIcon={<OpenInNewIcon />} variant="contained" fullWidth={true}>
-      View All Clips
-    </Button>
+    <Grid item={true} mt={1}>
+      <Button
+        startIcon={<OpenInNewIcon />}
+        variant="contained"
+        fullWidth={true}
+      >
+        View All Clips
+      </Button>
+    </Grid>
   );
 }
