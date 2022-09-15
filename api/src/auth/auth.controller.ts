ef3d9f29fbe7b406aee526be84ecf42ee00d9dbd
christophergeiger3 <christophergeiger3@gmail.com>
2022-09-14 21:43:06 -0400

    Add /auth/profile endpoint for JWT authentication testing

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { Request as RequestType } from 'express';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { User } from 'src/users/schema/user.schema';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -18,5 +26,11 @@ export class AuthController {
   @Post('/register')
   async register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/profile')
+  getStatus(@Request() req: RequestType & { user: User }) {
+    return { username: req.user.username };
   }
 }
