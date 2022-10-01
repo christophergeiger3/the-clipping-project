a59643643325e68c3e8069b6645b5c2fbd12c654
christophergeiger3 <christophergeiger3@gmail.com>
2022-10-01 19:32:24 -0400

    Sketch `<AllClipsView />`

--- vite/src/Routes.tsx
+++ vite/src/Routes.tsx
@@ -1,11 +1,13 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import AllClipsView from "./components/AllClipsView/AllClipsView";
 import ClippingView from "@components/ClippingView/ClippingView";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClippingView />} />
+        <Route path="/clips" element={<AllClipsView />} />
       </Routes>
     </BrowserRouter>
   );
