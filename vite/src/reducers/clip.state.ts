4310149859d7c7b4ffbe3b6d2e60f61ac00b68c7
Christopher Geiger <christophergeiger3@gmail.com>
2022-11-03 20:09:37 -0400

    Update ClipState constructor and defaults

--- vite/src/reducers/clip.state.ts
+++ vite/src/reducers/clip.state.ts
@@ -4,24 +4,22 @@ export const DEFAULT_VIDEO_SRC =
   "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
 export default class ClipState {
-  start: number;
-  end: number;
-  duration: number;
-  sliderMin: number;
-  sliderMax: number;
+  start = 0;
+  end = 0;
+  duration = 0;
+  sliderMin = 0;
+  sliderMax = this.duration;
   /** Raw video source for the video player -- the result from analyzing the original url */
-  src: string;
+  src = DEFAULT_VIDEO_SRC;
   /** The URL of the current clip before its analyzed into a raw video source for the player */
-  originalUrl: string;
+  originalUrl = this.src;
   player?: VideoJsPlayer;
 
   constructor(initialState?: ClipState) {
-    this.duration = initialState?.duration ?? 0;
-    this.start = initialState?.start ?? 0;
-    this.end = initialState?.end ?? this.duration;
-    this.sliderMin = initialState?.sliderMin ?? 0;
-    this.sliderMax = initialState?.sliderMax ?? this.duration;
-    this.src = initialState?.src || DEFAULT_VIDEO_SRC;
-    this.originalUrl = initialState?.originalUrl || this.src;
+    if (!initialState) {
+      return;
+    }
+
+    Object.assign(this, initialState);
   }
 }
