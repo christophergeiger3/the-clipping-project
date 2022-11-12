710b358c1d4df7d32ee94ca53669eb282310e638
Christopher Geiger <christophergeiger3@gmail.com>
2022-11-12 17:10:25 -0500

    Add bin/update.sh

---
+++ bin/update.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+PRE_RELEASE_URL="https://api.github.com/repos/christophergeiger3/the-clipping-project/releases/71234724"
+# RELEASE_URL="https://api.github.com/repos/christophergeiger3/the-clipping-project/releases/latest"
+
+navigate_to_script_directory() {
+    cd "$(dirname "$0")"
+}
+
+navigate_to_root() {
+    navigate_to_script_directory
+    cd ..
+}
+
+get_latest_release_tarball_url() {
+    # For now we're using the pre-release tarball since the latest release hasn't come out yet
+    tarball_url=$(curl -s "$PRE_RELEASE_URL" | grep tarball_url | cut -d : -f 2,3 | tr -d \" | tr -d ,)
+}
+
+download_latest_release_tarball() {
+    echo "Downloading the latest release tarball..."
+    get_latest_release_tarball_url
+    echo $tarball_url
+    wget -q $tarball_url -O latest.tar.gz
+}
+
+untar_latest_release_tarball() {
+    echo "Untarring the latest release tarball..."
+    # Untars to the current directory
+    tar -xzf latest.tar.gz --strip-components=1
+}
+
+remove_latest_release_tarball() {
+    echo "Removing the latest release tarball..."
+    rm latest.tar.gz
+}
+
+navigate_to_root
+download_latest_release_tarball
+untar_latest_release_tarball
+remove_latest_release_tarball
+echo "Done."
