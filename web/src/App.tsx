0b078f623718c1767d9dd064c6cac85539e2ec38
Christopher Geiger <christophergeiger3@gmail.com>
2022-12-27 19:49:41 -0500

    Wrap `<App />` in `<SnackbarProvider />`

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./providers/AuthProvider";
 import AppRoutes from "./Routes";
 import { API_URL } from "./env";
+import { SnackbarProvider } from "notistack";
 
 // Required for orval client requests (api.ts):
 Axios.defaults.baseURL = API_URL;
@@ -27,16 +28,18 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <AuthProvider>
-          <ClipProvider>
-            <AppRoutes />
-          </ClipProvider>
-        </AuthProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <SnackbarProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <AuthProvider>
+            <ClipProvider>
+              <AppRoutes />
+            </ClipProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </SnackbarProvider>
   );
 }
 
