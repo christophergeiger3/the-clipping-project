c2d77726ddcbfa09a6cbec433a8affc58e7a32be
Christopher Geiger <christophergeiger3@gmail.com>
2023-02-10 20:11:30 -0500

    Update comment

--- api/src/clips/utils/basename.ts
+++ api/src/clips/utils/basename.ts
@@ -1,5 +1,5 @@
 /** Return filename without extension (not to be confused with GNU's basename)
- * @example basename('file.ext') // 'file'
+ * @example basename('file.ext') // returns 'file'
  */
 export default function basename(filename: string): string {
   return filename.split('.').slice(0, -1).join('.');
